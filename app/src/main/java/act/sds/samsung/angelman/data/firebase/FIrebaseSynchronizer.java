7afc13a35c73104df8c8869623b64971833fd94a
Kate Ma and Milo Kim <pair+kate+milo@gmail.com>
2017-03-10 17:19:59 +0900

    temporary commit

--- app/src/main/java/act/sds/samsung/angelman/data/firebase/FIrebaseSynchronizer.java
+++ app/src/main/java/act/sds/samsung/angelman/data/firebase/FIrebaseSynchronizer.java
@@ -1,58 +1,66 @@
 package act.sds.samsung.angelman.data.firebase;
 
+import android.content.Context;
+import android.net.Uri;
+
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.File;
 import java.util.List;
 
+import act.sds.samsung.angelman.AngelmanApplication;
 import act.sds.samsung.angelman.domain.model.CardModel;
 import act.sds.samsung.angelman.domain.model.CategoryModel;
 
 public class FirebaseSynchronizer {
 
 
-    private DatabaseReference categoryDatabase ;
-    private DatabaseReference cardDatabase ;
+    private DatabaseReference categoryDatabase;
+    private DatabaseReference cardDatabase;
+    private StorageReference storageReference;
 
-    private static FirebaseSynchronizer instance;
 
-    private FirebaseSynchronizer () {
+    private Context context;
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        categoryDatabase = database.getReference("Category");
-        cardDatabase = database.getReference("Card");
-
+    public FirebaseSynchronizer(Context context) {
+        this.context = context;
     }
 
-    public static FirebaseSynchronizer getInstance () {
 
-        if(instance == null){
-            instance = new FirebaseSynchronizer();
-            return instance;
-        }
-        return instance;
-
+    private FirebaseSynchronizer() {
+        categoryDatabase = FirebaseDatabase.getInstance().getReference("Category");
+        cardDatabase = FirebaseDatabase.getInstance().getReference("Card");
+        storageReference = FirebaseStorage.getInstance().getReference();
     }
 
-    public void uploadDataToFirebase(List<CategoryModel> categoryModelList , List<CardModel> cardModelList){
-
-        //String userId = sharedPreferences.get("USER_ID"); // 이런식으로 동작이 이루어져야함
+    public void uploadDataToFirebase(List<CategoryModel> categoryModelList, List<CardModel> cardModelList) {
         String userId = "MILO";
 
+        uploadData(categoryModelList, cardModelList, userId);
 
-        //upload category
-        for( CategoryModel categoryModel : categoryModelList){
+        //File upload
+        String voicesFolderPath = ((AngelmanApplication) context.getApplicationContext()).getVoiceFolder();
+        String imagesFolderPath = ((AngelmanApplication) context.getApplicationContext()).getImageFolder();
+
+        Uri folder = Uri.fromFile(new File());
+        StorageReference voiceReference = storageReference.child(userId).child("voice/");
+        voiceReference.putFile(Uri.parse(voicesFolderPath));
+
+
+
+    }
+
+    private void uploadData(List<CategoryModel> categoryModelList, List<CardModel> cardModelList, String userId) {
+        for (CategoryModel categoryModel : categoryModelList) {
             categoryDatabase.child(userId).child(categoryModel._id).setValue(categoryModel);
         }
-
-        //upload card
-        for( CardModel cardModel : cardModelList){
+        for (CardModel cardModel : cardModelList) {
             cardDatabase.child(userId).child(cardModel._id).setValue(cardModel);
         }
     }
-
-
-
 
 
 }
