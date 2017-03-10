69d713bb51cc553405061d3379646ea351abf595
Kate Ma and Milo Kim <pair+kate+milo@gmail.com>
2017-03-10 18:47:46 +0900

    Firebase Synchronizer update

--- app/src/main/java/act/sds/samsung/angelman/dagger/modules/AngelmanModule.java
+++ app/src/main/java/act/sds/samsung/angelman/dagger/modules/AngelmanModule.java
@@ -6,8 +6,9 @@ import javax.inject.Singleton;
 
 import act.sds.samsung.angelman.data.repository.CardDataRepository;
 import act.sds.samsung.angelman.data.repository.CategoryDataRepository;
-import act.sds.samsung.angelman.domain.repository.CategoryRepository;
+import act.sds.samsung.angelman.data.synchronizer.FirebaseSynchronizer;
 import act.sds.samsung.angelman.domain.repository.CardRepository;
+import act.sds.samsung.angelman.domain.repository.CategoryRepository;
 import dagger.Module;
 import dagger.Provides;
 
