103baf87238192ba6c5d4861bc7b2a9ba1e1178b
Lamos Kim and Steve Jeong <pair+lamos+steve@gmail.com>
2017-04-13 18:53:47 +0900

    UItest fix.
    Former-commit-id: 4011ab31bd83b6ba478a1c1e2a65864c0489bddf

--- app/src/androidTest/java/act/sds/samsung/angelman/presentation/activity/OnboardingAndCategoryMenuViewTest.java
+++ app/src/androidTest/java/act/sds/samsung/angelman/presentation/activity/OnboardingAndCategoryMenuViewTest.java
@@ -1,31 +1,31 @@
 package act.sds.samsung.angelman.presentation.activity;
 
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Resources;
-import android.support.test.espresso.ViewInteraction;
+import android.support.annotation.NonNull;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 
-import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import act.sds.samsung.angelman.R;
+import act.sds.samsung.angelman.presentation.manager.ApplicationConstants;
 
+import static act.sds.samsung.angelman.presentation.activity.TestUtil.childAtPosition;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 
@@ -36,78 +36,56 @@ public class OnboardingAndCategoryMenuViewTest {
     @Rule
     public ActivityTestRule<OnboardingActivity> mActivityTestRule = new ActivityTestRule<>(OnboardingActivity.class);
 
+    @Before
+    public void setUp() throws Exception {
+        SharedPreferences preferences = mActivityTestRule.getActivity().getApplicationContext().getSharedPreferences(ApplicationConstants.PRIVATE_PREFERENCE_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor edit = preferences.edit();
+        edit.putBoolean(ApplicationConstants.FIRST_LAUNCH, true);
+        edit.commit();
+    }
+
     @Test
     public void onboardingAndCategoryMenuViewTest() throws InterruptedException {
 
         Resources resources = mActivityTestRule.getActivity().getResources();
 
-        onView(
-                allOf(
-                        withId(R.id.onboarding_start),
-                        withParent(withId(R.id.onboarding_first_page))
-                )
-        ).check(matches(isDisplayed()));
+        onView(withId(R.id.onboarding_start))
+                .check(matches(isDisplayed()));
 
-        Thread.sleep(4000);
+        Thread.sleep(4000); // wait for flying angel
 
-        onView(allOf(withId(R.id.onboarding_view_pager)))
+        onView(withId(R.id.onboarding_view_pager))
                 .perform(swipeLeft())
                 .perform(swipeLeft())
                 .perform(swipeLeft())
                 .perform(swipeLeft());
 
-        ViewInteraction onBoardingFinishButton = onView(allOf(withId(R.id.onboarding_finish)));
-        onBoardingFinishButton.check(matches(isDisplayed()));
-        onBoardingFinishButton.perform(click());
+        onView(withId(R.id.onboarding_finish))
+                .check(matches(isDisplayed()))
+                .perform(click());
 
-        ViewInteraction sendVocButton = onView(
-                allOf(
-                        withId(R.id.send_voc),
-                        withParent(withId(R.id.clock_layout))
-                )
-        );
-        sendVocButton.check(matches(isDisplayed()));
-
-        ViewInteraction categoryDeleteButton = onView(
-                allOf(
-                        withId(R.id.category_delete_button),
-                        withParent(withId(R.id.clock_layout))
-                )
-        );
-        categoryDeleteButton.check(matches(isDisplayed()));
-        categoryDeleteButton.check(matches(withText(resources.getString(R.string.delete))));
-
-        ViewInteraction newCategoryItemView = onView(
-                allOf(
-                        withId(R.id.category_title),
-                        childAtPosition(childAtPosition(childAtPosition(childAtPosition(
-                                childAtPosition(
-                                        withId(R.id.category_list),
-                                        5),
-                                0),0),1),1
-                        )
-                )
-        );
-        newCategoryItemView.check(matches(isDisplayed()));
-        newCategoryItemView.check(matches(withText(resources.getString(R.string.new_category))));
+        onView(withId(R.id.send_voc))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.logo_angeltalk))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.category_delete_button))
+                .check(matches(isDisplayed()))
+                .check(matches(withText("삭제")));
+        onView(newCategoryItemTitleMatcher())
+            .check(matches(isDisplayed()))
+            .check(matches(withText(resources.getString(R.string.new_category))));
     }
 
-    private static Matcher<View> childAtPosition(
-            final Matcher<View> parentMatcher, final int position) {
-
-        return new TypeSafeMatcher<View>() {
-            @Override
-            public void describeTo(Description description) {
-                description.appendText("Child at position " + position + " in parent ");
-                parentMatcher.describeTo(description);
-            }
-
-            @Override
-            public boolean matchesSafely(View view) {
-                ViewParent parent = view.getParent();
-                return parent instanceof ViewGroup && parentMatcher.matches(parent)
-                        && view.equals(((ViewGroup) parent).getChildAt(position));
-            }
-        };
+    @NonNull
+    private Matcher<View> newCategoryItemTitleMatcher() {
+        return allOf(
+                withId(R.id.category_title),
+                childAtPosition(childAtPosition(childAtPosition(childAtPosition(
+                        childAtPosition(
+                                withId(R.id.category_list),
+                                5),
+                        0),0),1),1
+                )
+        );
     }
 }
