da834d8138252f1582a580cb2dc8955749cc83e6
Kate Ma and Lamos Kim <pair+kate+lamos@gmail.com>
2017-04-24 14:42:26 +0900

    refactoring dialogs
    Former-commit-id: 67a0be21521b0bd40baf9e950f31da1c9fa69379

--- app/src/main/java/act/angelman/presentation/custom/CustomConfirmDialog.java
+++ app/src/main/java/act/angelman/presentation/custom/CustomConfirmDialog.java
@@ -7,21 +7,27 @@ import android.view.View;
 import android.widget.TextView;
 
 import act.angelman.R;
-import act.angelman.presentation.util.ContentsUtil;
 import act.angelman.presentation.util.DialogUtil;
 
 public class CustomConfirmDialog {
 
-    private static final int DEFAULT_WIDTH = 310;
-    private static final int DEFAULT_HEIGHT = 235;
     private final AlertDialog dialog;
+    private final Context context;
 
-    public CustomConfirmDialog(Context context, String message, View.OnClickListener positiveOnClickListener, View.OnClickListener negativeOnClickListener) {
+    public CustomConfirmDialog(Context context, String message, View.OnClickListener positiveOnClickListener) {
+        this.context = context;
         View innerView = ((Activity)context).getLayoutInflater().inflate(R.layout.custom_confirm_dialog, null);
         ((TextView) innerView.findViewById(R.id.alert_message)).setText(message);
-        dialog = DialogUtil.buildCustomDialog(context, innerView, positiveOnClickListener, negativeOnClickListener);
-        dialog.show();
-        dialog.getWindow().setLayout((int) ContentsUtil.convertDpToPixel(DEFAULT_WIDTH, context),(int) ContentsUtil.convertDpToPixel(DEFAULT_HEIGHT, context));
+        dialog = DialogUtil.buildCustomDialog(context, innerView, positiveOnClickListener, new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dismiss();
+            }
+        });
+    }
+
+    public void show() {
+        DialogUtil.show(context, dialog);
     }
 
     public void dismiss() {
