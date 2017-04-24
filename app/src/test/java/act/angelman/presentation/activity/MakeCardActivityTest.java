d80709c96363d62f4607c23c25a3925c1b51c956
Lamos Kim and Milo Kim <pair+lamos+milo@gmail.com>
2017-04-24 18:31:41 +0900

    temporary commit
    Former-commit-id: 001f7c09738dc360a27c56f7ef5eaea5186c2a0d [formerly 4b9a2fb405e22a30739143c4a23949a6e51189d3]
    Former-commit-id: eaa840f9b43884ef4bdf01c754c9075e10e6f78e

--- app/src/test/java/act/angelman/presentation/activity/MakeCardActivityTest.java
+++ app/src/test/java/act/angelman/presentation/activity/MakeCardActivityTest.java
@@ -87,17 +87,29 @@ public class MakeCardActivityTest extends UITest{
         subject = setupActivityWithIntent(MakeCardActivity.class, intent);
 
     }
-    private void setupEditCard() {
+    private void setupEditCardForRenaming() {
         Intent intent = new Intent();
         intent.putExtra(ApplicationConstants.EDIT_CARD_ID, "1");
+        intent.putExtra(ApplicationConstants.EDIT_TYPE, ApplicationConstants.CardEditType.NAME.value());
         CardModel editCardModel = CardModel.builder()._id("1").name("버스")
                 .contentPath("/Users/ssa009/workspace/angelman/app/src/main/assets/bus.jpg")
                 .cardType(CardModel.CardType.PHOTO_CARD)
                 .build();
         when(cardRepository.getSingleCard(anyString())).thenReturn(editCardModel);
-        when(applicationManager.getCategoryModel()).thenReturn(getCategoryModel(
-
-        ));
+        when(applicationManager.getCategoryModel()).thenReturn(getCategoryModel());
+        when(applicationManager.getCategoryModelColor()).thenReturn(getCategoryModelColor());
+        subject = setupActivityWithIntent(MakeCardActivity.class, intent);
+    }
+    private void setupEditCardForVoiceChange() {
+        Intent intent = new Intent();
+        intent.putExtra(ApplicationConstants.EDIT_CARD_ID, "1");
+        intent.putExtra(ApplicationConstants.EDIT_TYPE, ApplicationConstants.CardEditType.VOICE.value());
+        CardModel editCardModel = CardModel.builder()._id("1").name("버스")
+                .contentPath("/Users/ssa009/workspace/angelman/app/src/main/assets/bus.jpg")
+                .cardType(CardModel.CardType.PHOTO_CARD)
+                .build();
+        when(cardRepository.getSingleCard(anyString())).thenReturn(editCardModel);
+        when(applicationManager.getCategoryModel()).thenReturn(getCategoryModel());
         when(applicationManager.getCategoryModelColor()).thenReturn(getCategoryModelColor());
         subject = setupActivityWithIntent(MakeCardActivity.class, intent);
     }
@@ -180,7 +192,7 @@ public class MakeCardActivityTest extends UITest{
 
     @Test
     public void givenEditCard_whenCompleteEditing_thenUpdateCardModelAndMoveToCategoryViewPagerActivity() throws Exception {
-        setupEditCard();
+        setupEditCardForRenaming();
 
         EditText cardTitleEdit = (EditText) subject.findViewById(R.id.card_image_title_edit);
         cardTitleEdit.setText("TEST");
@@ -447,8 +459,15 @@ public class MakeCardActivityTest extends UITest{
 
     @Test
     public void givenEditCardId_whenLaunched_thenGetSingleCardWithCardId() throws Exception {
-        setupEditCard();
+        setupEditCardForRenaming();
         verify(cardRepository).getSingleCard(anyString());
+    }
+
+    @Test
+    public void given() throws Exception {
+
+        setupEditCardForVoiceChange();
+
     }
 
     private void recordComplete() {
