d9e32448f0808bddae62b68c7b594991e2f21cd3
Lamos Kim and Milo Kim <pair+lamos+milo@gmail.com>
2017-05-15 15:21:59 +0900

    add unit test to CategoryDataRepositoryTest
    Former-commit-id: 689bf775d7a137e7a7d970a73cfaf7ca2234a802

--- app/src/main/java/act/angelman/data/repository/CategoryDataRepository.java
+++ app/src/main/java/act/angelman/data/repository/CategoryDataRepository.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import java.util.List;
 
 import act.angelman.data.repository.datastore.CategoryDataSqliteDataStore;
+import act.angelman.data.repository.datastore.CategoryDataStore;
 import act.angelman.domain.model.CategoryItemModel;
 import act.angelman.domain.model.CategoryModel;
 import act.angelman.domain.repository.CategoryRepository;
@@ -12,38 +13,35 @@ import act.angelman.domain.repository.CategoryRepository;
 public class CategoryDataRepository implements CategoryRepository {
 
     private Context context;
+    CategoryDataStore dataStore;
 
     public CategoryDataRepository(Context context) {
         this.context = context;
+        this.dataStore = new CategoryDataSqliteDataStore(context);
     }
 
     @Override
     public List<CategoryModel> getCategoryAllList() {
-        CategoryDataSqliteDataStore dataStore = new CategoryDataSqliteDataStore(context);
         return dataStore.getCategoryAllList();
     }
 
     @Override
     public List<CategoryItemModel> getCategoryAllIconList() {
-        CategoryDataSqliteDataStore dataStore = new CategoryDataSqliteDataStore(context);
         return dataStore.getCategoryAllIconList();
     }
 
     @Override
     public List<CategoryItemModel> getCategoryAllBackgroundList() {
-        CategoryDataSqliteDataStore dataStore = new CategoryDataSqliteDataStore(context);
         return dataStore.getCategoryAllBackgroundList();
     }
 
     @Override
     public int saveNewCategoryItemAndReturnId(CategoryModel model) {
-        CategoryDataSqliteDataStore dataStore = new CategoryDataSqliteDataStore(context);
         return dataStore.saveNewCategoryItemAndReturnId(model);
     }
 
     @Override
     public boolean deleteCategory(int category) {
-        CategoryDataSqliteDataStore dataStore = new CategoryDataSqliteDataStore(context);
         return dataStore.deleteCategory(category);
     }
 
