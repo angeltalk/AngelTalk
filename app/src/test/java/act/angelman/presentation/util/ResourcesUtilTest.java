4c4f5816d3cca0adbea3724f3af228dffc5dc1dd
Lamos Kim and Steve Jeong <pair+lamos+steve@gmail.com>
2017-05-23 14:17:41 +0900

    add ResourcesUtilTest code
    Former-commit-id: c8e478d2ab5c02a37dcbcf0d83c8941e40cb463c [formerly e539a86a5c43748466ddebb2a9cab6a5b2d1778c]
    Former-commit-id: d0504a315702831f172ec16b0471d662ad4874a2

--- app/src/test/java/act/angelman/presentation/util/ResourcesUtilTest.java
+++ app/src/test/java/act/angelman/presentation/util/ResourcesUtilTest.java
@@ -1,46 +1,64 @@
 package act.angelman.presentation.util;
 
 import android.content.Context;
+import android.view.View;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
 import org.robolectric.annotation.Config;
 
 import act.angelman.BuildConfig;
 import act.angelman.R;
+import act.angelman.presentation.shadow.ShadowResourcesCompat;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 @RunWith(RobolectricTestRunner.class)
-@Config(constants = BuildConfig.class)
+@Config(constants = BuildConfig.class, shadows = {ShadowResourcesCompat.class})
 public class ResourcesUtilTest {
 
-    private ResourcesUtil subject;
 
     @Test
     public void setColorThemeTest() throws Exception {
-        subject = new ResourcesUtil();
         Context context = mock(Context.class);
 
-        subject.setColorTheme(context, ResourcesUtil.RED);
+        ResourcesUtil.setColorTheme(context, ResourcesUtil.RED);
         verify(context).setTheme(eq(R.style.AppTheme_Red));
 
-        subject.setColorTheme(context, ResourcesUtil.ORANGE);
+        ResourcesUtil.setColorTheme(context, ResourcesUtil.ORANGE);
         verify(context).setTheme(eq(R.style.AppTheme_Orange));
 
-        subject.setColorTheme(context, ResourcesUtil.YELLOW);
+        ResourcesUtil.setColorTheme(context, ResourcesUtil.YELLOW);
         verify(context).setTheme(eq(R.style.AppTheme_Yellow));
 
-        subject.setColorTheme(context, ResourcesUtil.GREEN);
+        ResourcesUtil.setColorTheme(context, ResourcesUtil.GREEN);
         verify(context).setTheme(eq(R.style.AppTheme_Green));
 
-        subject.setColorTheme(context, ResourcesUtil.BLUE);
+        ResourcesUtil.setColorTheme(context, ResourcesUtil.BLUE);
         verify(context).setTheme(eq(R.style.AppTheme_Blue));
 
-        subject.setColorTheme(context, ResourcesUtil.PURPLE);
+        ResourcesUtil.setColorTheme(context, ResourcesUtil.PURPLE);
         verify(context).setTheme(eq(R.style.AppTheme_Purple));
+    }
+
+    @Test
+    public void getCardViewLayoutBackgroundByTest() throws Exception {
+        assertThat(ResourcesUtil.getCardViewLayoutBackgroundBy(ResourcesUtil.RED)).isEqualTo(R.drawable.background_gradient_red);
+        assertThat(ResourcesUtil.getCardViewLayoutBackgroundBy(ResourcesUtil.ORANGE)).isEqualTo(R.drawable.background_gradient_orange);
+        assertThat(ResourcesUtil.getCardViewLayoutBackgroundBy(ResourcesUtil.YELLOW)).isEqualTo(R.drawable.background_gradient_yellow);
+        assertThat(ResourcesUtil.getCardViewLayoutBackgroundBy(ResourcesUtil.GREEN)).isEqualTo(R.drawable.background_gradient_green);
+        assertThat(ResourcesUtil.getCardViewLayoutBackgroundBy(ResourcesUtil.BLUE)).isEqualTo(R.drawable.background_gradient_blue);
+        assertThat(ResourcesUtil.getCardViewLayoutBackgroundBy(ResourcesUtil.PURPLE)).isEqualTo(R.drawable.background_gradient_purple);
+    }
+
+    @Test
+    public void setViewBackgroundTest() throws Exception {
+        ResourcesUtil.setViewBackground(mock(View.class), ResourcesUtil.PURPLE, RuntimeEnvironment.application.getApplicationContext());
+        assertThat(ShadowResourcesCompat.capturedId).isEqualTo(R.drawable.background_gradient_purple);
     }
 }
