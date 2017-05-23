bdd5c81815c402e42ed1e6f76e43ee6dd073ffc2
Lamos Kim and Steve Jeong <pair+lamos+steve@gmail.com>
2017-05-23 13:45:04 +0900

    add ResourcesUtilTest code
    Former-commit-id: 6fd37803f4de943530829546bf7a5624367512c5 [formerly 540d728acbc81bd730e679576900a98de50545e3]
    Former-commit-id: b4a3d86b90380441999c5f4069d0749cb9350c4d

--- app/src/test/java/act/angelman/presentation/util/ResourcesUtilTest.java
+++ app/src/test/java/act/angelman/presentation/util/ResourcesUtilTest.java
@@ -1,10 +1,46 @@
 package act.angelman.presentation.util;
 
-import static org.junit.Assert.*;
+import android.content.Context;
 
-/**
- * Created by devcraft47 on 2017. 5. 23..
- */
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import act.angelman.BuildConfig;
+import act.angelman.R;
+
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class)
 public class ResourcesUtilTest {
 
+    private ResourcesUtil subject;
+
+    @Test
+    public void setColorThemeTest() throws Exception {
+        subject = new ResourcesUtil();
+        Context context = mock(Context.class);
+
+        subject.setColorTheme(context, ResourcesUtil.RED);
+        verify(context).setTheme(eq(R.style.AppTheme_Red));
+
+        subject.setColorTheme(context, ResourcesUtil.ORANGE);
+        verify(context).setTheme(eq(R.style.AppTheme_Orange));
+
+        subject.setColorTheme(context, ResourcesUtil.YELLOW);
+        verify(context).setTheme(eq(R.style.AppTheme_Yellow));
+
+        subject.setColorTheme(context, ResourcesUtil.GREEN);
+        verify(context).setTheme(eq(R.style.AppTheme_Green));
+
+        subject.setColorTheme(context, ResourcesUtil.BLUE);
+        verify(context).setTheme(eq(R.style.AppTheme_Blue));
+
+        subject.setColorTheme(context, ResourcesUtil.PURPLE);
+        verify(context).setTheme(eq(R.style.AppTheme_Purple));
+    }
 }
