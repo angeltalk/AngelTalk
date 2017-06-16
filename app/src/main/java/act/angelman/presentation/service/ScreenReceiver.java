042716f47c490b21b842c351be6fd33d3bf5e4d5
Steve Jeong and Woody Kwon <pair+steve+woody@gmail.com>
2017-06-16 10:58:26 +0900

    [AN-279] fix app crash bug when making a child mode layout without overlay permission
    Former-commit-id: 8c1200741eb94d2c2bbc60301f0497bc44c0bae9

--- app/src/main/java/act/angelman/presentation/service/ScreenReceiver.java
+++ app/src/main/java/act/angelman/presentation/service/ScreenReceiver.java
@@ -4,11 +4,15 @@ import android.app.KeyguardManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
 
 import javax.inject.Inject;
 
 import act.angelman.AngelmanApplication;
 import act.angelman.presentation.manager.ApplicationManager;
+
+import static android.os.Build.VERSION_CODES.M;
 
 
 public class ScreenReceiver extends BroadcastReceiver {
@@ -21,6 +25,10 @@ public class ScreenReceiver extends BroadcastReceiver {
 
     public void onReceive(final Context context, Intent intent) {
         ((AngelmanApplication) context.getApplicationContext()).getAngelmanComponent().inject(this);
+
+        if (Build.VERSION.SDK_INT >= M && !Settings.canDrawOverlays(context)) {
+            return;
+        }
 
         if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
 
