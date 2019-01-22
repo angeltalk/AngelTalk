044a0aeebedf7a09f8b6a1762c9c9af8a6ce90a6
actmember <Jeep8walrus!>
2019-01-22 11:15:10 +0900

    add build.sh, deploy.sh

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,22 @@
 #./gradlew clean createDebugCoverageReport jacocoTestReport sonarqube
-./gradlew clean build coverageReport
+./gradlew clean build coverageReport exportVersionName
+
+#-Pandroid.injected.signing.store.file="travis-encrypt/calysigningkey.jks" \
+#-Pandroid.injected.signing.store.password=$STORE_PASSWORD \
+#-Pandroid.injected.signing.key.alias=$KEY_ALIAS \
+#-Pandroid.injected.signing.key.password=$KEY_PASSWORD
+
+file_date=$(date "+%m.%d-%H:%M")
+version_name=$(VERSION_NAME)
+
+#debug
+file_name_debug="app/build/outputs/apk/debug/AngelTalk-$version_name-debug.apk"
+file_location_debug="app/build/outputs/apk/debug/AngelTalk-$version_name-debug-$file_date.apk"
+
+cp $file_location_debug $file_name_debug
+
+#release
+file_name_release="app/build/outputs/apk/release/AngelTalk-$version_name-release.apk"
+file_location_release="app/build/outputs/apk/release/AngelTalk-$version_name-release-$file_date.apk"
+
+cp $file_location_release $file_name_release
---
+++ script/deploy.sh
@@ -0,0 +1,45 @@
+#get commit info
+#if [ -z "$TRAVIS_BRANCH" ]
+#  then
+#    echo "deploy in local"
+    DEPLOY_BRANCH=$(git symbolic-ref --short HEAD)
+    DEPLOY_COMMIT=$(git log --pretty=format:'%h' -n 1)
+    DEPLOY_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+#  else
+#    echo "deploy in travis"
+#    DEPLOY_BRANCH="$TRAVIS_BRANCH"
+#    DEPLOY_COMMIT=$(git log --pretty=format:'%h' -n 1)
+#    DEPLOY_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+#fi
+
+echo $DEPLOY_BRANCH
+echo $DEPLOY_COMMIT
+echo $DEPLOY_COMMIT_MESSAGE
+
+version_name=$(VERSION_NAME)
+
+#: <<'END'
+
+#debug
+filename="$(find . -name AngelTalk-$version_name-debug-*.apk)"
+
+SLACK_TEXT="[ *DEBUG* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${DEPLOY_COMMIT_MESSAGE:-none} "
+curl \
+  -F "token=$SLACK_KEY" \
+  -F "channels=deploy-android" \
+  -F "initial_comment=$SLACK_TEXT" \
+  -F "file=@$filename" \
+  https://slack.com/api/files.upload
+
+#release
+filename="$(find . -name AngelTalk-$version_name-release-*.apk)"
+
+SLACK_TEXT="[ *RELEASE* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${DEPLOY_COMMIT_MESSAGE:-none} "
+curl \
+  -F "token=$SLACK_KEY" \
+  -F "channels=deploy-android" \
+  -F "initial_comment=$SLACK_TEXT" \
+  -F "file=@$filename" \
+  https://slack.com/api/files.upload
+
+#END
