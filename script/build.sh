5daf84cf59c219a1fec1a41b6a1748b2be1d8a12
actmember <Jeep8walrus!>
2019-01-22 18:15:25 +0900

    - fix build.gradle to append date to apk file name

--- script/build.sh
+++ script/build.sh
@@ -5,17 +5,3 @@
 #-Pandroid.injected.signing.store.password=$STORE_PASSWORD \
 #-Pandroid.injected.signing.key.alias=$KEY_ALIAS \
 #-Pandroid.injected.signing.key.password=$KEY_PASSWORD
-
-file_date=$(date "+%m.%d-%H:%M")
-
-#debug
-file_name_debug="app/build/outputs/apk/debug/AngelTalk-debug.apk"
-file_location_debug="app/build/outputs/apk/debug/AngelTalk-debug-$file_date.apk"
-
-cp $file_location_debug $file_name_debug
-
-#release
-file_name_release="app/build/outputs/apk/release/AngelTalk-release.apk"
-file_location_release="app/build/outputs/apk/release/AngelTalk-release-$file_date.apk"
-
-cp $file_location_release $file_name_release
