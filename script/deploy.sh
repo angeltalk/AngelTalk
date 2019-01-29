fbd1852f028acd084f4c187c99cc3d550470bdcd
actmember <Jeep8walrus!>
2019-01-29 14:45:51 +0900

    modify deploy.sh

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,43 +1,8 @@
-#get commit info
-#if [ -z "$TRAVIS_BRANCH" ]
-#  then
-#    echo "deploy in local"
-#    DEPLOY_BRANCH=$(git symbolic-ref --short HEAD)
-#    DEPLOY_COMMIT=$(git log --pretty=format:'%h' -n 1)
-#    DEPLOY_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
-#  else
-#    echo "deploy in travis"
-#    DEPLOY_BRANCH="$TRAVIS_BRANCH"
-#    DEPLOY_COMMIT=$(git log --pretty=format:'%h' -n 1)
-#    DEPLOY_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
-#fi
-
-#echo $DEPLOY_BRANCH
-#echo $DEPLOY_COMMIT
-#echo $DEPLOY_COMMIT_MESSAGE
-
-#: <<'END'
-
-#debug
 filename="$(find . -name AngelTalk-*-debug-*.apk)"
 
-#SLACK_TEXT="[ *DEBUG* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${DEPLOY_COMMIT_MESSAGE:-none} "
 curl \
   -F "token=$SLACK_KEY" \
   -F "channels=apk_from_travis" \
-  -F "initial_comment=$SLACK_TEXT" \
+  -F "initial_comment=Apk file of AngelTalk is created." \
   -F "file=@$filename" \
   https://slack.com/api/files.upload
-
-#release
-#filename="$(find . -name AngelTalk-*-release-*.apk)"
-
-#SLACK_TEXT="[ *RELEASE* \`$DEPLOY_BRANCH\` | \`$DEPLOY_COMMIT\` ] ${DEPLOY_COMMIT_MESSAGE:-none} "
-#curl \
-#  -F "token=$SLACK_KEY" \
-#  -F "channels=apk_from_travis" \
-#  -F "initial_comment=$SLACK_TEXT" \
-#  -F "file=@$filename" \
-#  https://slack.com/api/files.upload
-
-#END
